f05c1a622df82735fce349a8b8a3ee1c58a6fac1
caraido <254130357@qq.com>
2020-10-22 11:09:01 -0500

    Build a skeleton for video processing

---
+++ video_processing.py
@@ -0,0 +1,51 @@
+import cv2
+import PySpin
+import numpy as np
+
+
+class CharucoBoard:
+    def __init__(self,grid_num,):
+        pass
+
+    def create_baord(self):
+        pass
+
+    def save_board(self):
+        pass
+
+    def print_board(self):
+        pass
+
+
+class Camera:
+    def __init__(self):
+        pass
+
+    def get_all_cameras(self):
+        pass
+
+    def start_camera(self):
+        pass
+
+    def get_camera_property(self):
+        pass
+
+    def calibration(self):
+        pass
+
+    def acquire_image(self):
+        pass
+
+    def save_image(self):
+        pass
+
+    def save_video(self):
+        pass
+
+    def save_intrinsic(self):
+        pass
+
+    def save_extrinsic(self):
+        pass
+
+
